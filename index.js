8a359dd84d7cf4635175cf045dde4d3df6097532
Julien NOBLET <julien.noblet@gmail.com>
2014-03-17 07:07:30 -0400

    NeDB v0.0.1

--- index.js
+++ index.js
@@ -1,41 +1,52 @@
-// we use redback as it has some higher level functionality!
-var redback = require("redback")
+var Datastore = require('nedb')
 
+function now() { return (new Date()).getTime(); }
 
-function CacherRedis(port, host, opts) {
-  if (port instanceof redback.Client) {
-    this.client = redback.use(port)
-  }
-  opts = opts || {}
-
-  this.client = this.client || new redback.createClient(port, host, opts)
-
-  opts.namespace = opts.namespace || "cacher"
-  this.cache = this.client.createCache(opts.namespace)
+function CacherNeDB(filename) {
+  filename = filename || null
+  var db;
+  if (filename) 
+    db = new Datastore({ filename: filename, autoload: true });
+  else 
+    db = new Datastore();
+  db.persistence.setAutocompactionInterval(5000);
+  this.cache = db;
 }
 
-CacherRedis.prototype.get = function(key, cb) {
-  this.cache.get(key, function(err, obj) {
-    if (err) return cb(err)
-
-    try {
-      obj = JSON.parse(obj)
-    } catch (e) {
-    } finally {
-      cb(null, obj)
-    }
-  })
-}
-
-CacherRedis.prototype.set = function(key, cacheObject, ttl, cb) {
+CacherNeDB.prototype.set = function(key, cacheObject, ttl, cb) {
   cb = cb || function() {}
   if (typeof cacheObject == 'object') cacheObject = JSON.stringify(cacheObject)
-  this.cache.set(key, cacheObject, ttl, cb)
+  this.cache.update({ url: key }, {url :key, object : cacheObject, ttl: ttl*1000, start:now()}, { upsert: true }, cb);
 }
 
-CacherRedis.prototype.invalidate = function(key, cb) {
+CacherNeDB.prototype.get = function(key, cb) {
+  console.log('get key :'+key);
+  this.cache.findOne({ url: key }, function (err, doc) {
+    if (err) return cb(err)
+    try {
+      doc = JSON.parse(doc)
+    } catch (e) {
+    } finally {
+      if (doc===null)
+        cb(null, doc)
+      else{
+        if (now() <= doc.start + doc.ttl)
+          cb (null,JSON.parse(doc.object))
+        else{
+          cb (null,null)
+        }
+          
+      }
+    }
+    // If no document is found, doc is null
+  });
+}
+
+
+
+CacherNeDB.prototype.invalidate = function(key, cb) {
   cb = cb || function() {}
-  this.cache.flush(key, cb)
+  this.cache.remove({ url: key }, {}, cb);
 }
 
-module.exports = CacherRedis
+module.exports = CacherNeDB
